27e4480d66fa39f706d59e3f7e5b7caddf14275c
Ansh Anurag <anxhnurag@gmail.com>
2021-01-20 09:49:49 +0530

    Update binary search.c

--- binary search.c
+++ binary search.c
@@ -1,27 +1,47 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <stdio.h>
 
 int main()
 {
     int n;
     scanf("%d",&n);
-    int arr[n],low=0,high=n-1;
+    int arr[n],low=0,high=n-1,flag=1;
     int mid = (low+high)/2;
     for(int i=0; i<n ; i++)
         scanf("%d",&arr[i]);
-    scanf("%d",&n);
-    while(low!=mid && high!=mid)
+    int i;
+    scanf("%d",&i);
+    while(low!=high)
     {
-        if(n>arr[mid])
-            low=mid;
-        else if(n<arr[mid])
-            high=mid;
+        if(i>arr[mid])
+            low=mid+1;
+        else if(i<arr[mid])
+            high=mid-1;
         else
             {
                 low=mid;
                 high=mid;
             }
+        
+        
+        //printf("%d\n",low);printf("%d\n",mid);printf("%d\n",high);
         mid=(high+low)/2;
+        
+        if(low==high && i==arr[high])
+        {
+            flag=0;
+            break;
+        }
     }
-    printf("%d",mid);
-    return 0;
+    if (flag==0 )
+        printf("%d",mid);
+    else
+        printf("no soln");
 }
