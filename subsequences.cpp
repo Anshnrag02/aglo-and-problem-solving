da895a8650cb20611ae574e644cd65d7c2c79e16
Ansh Anurag <anxhnurag@gmail.com>
2021-04-16 22:17:06 +0530

    Create subsequences.cpp

---
+++ subsequences.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printer(char *a, int no){
+    int i=0;
+    while(no>0){
+        
+        (no&1)?cout<<a[i]:cout<<"";
+        i++;
+        no=no>>1;
+    }
+    
+}
+
+void subseq(char *a){
+    int n=strlen(a);
+    int range=(1<<n) - 1;
+    for(int i = 0 ; i < range ; i++){
+        printer(a,i);
+        cout<<endl;
+    }
+        
+}
+
+int main()
+{
+    char a[]="fuddu";
+    subseq(a);
+}
