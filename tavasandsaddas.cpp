276eb5f53d29495024ebb15a1dd94ef24b3a30bf
Ansh Anurag <anxhnurag@gmail.com>
2021-04-17 19:24:40 +0530

    Create tavasandsaddas.cpp

---
+++ tavasandsaddas.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int mult=10;
+    int sum=0;
+    int count=0;
+    while(n){
+        if(n%mult==4){
+            sum+=1<<count;
+        }
+        else
+            sum+=1<<(count+1);
+        count++;
+        mult*=10;
+        n/=10;
+    }
+    cout<<sum;
+    return 0;
+}
